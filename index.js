b8659e902bad86ab7f2fe3f7ffa86953e6520d6b
Adam Rudd <adam.rudd@uqconnect.edu.au>
2014-08-12 11:09:26 +0200

    Format errors and results

--- index.js
+++ index.js
@@ -21,7 +21,67 @@ Tradedoubler.prototype.done = function (cb) {
   return request
     .get(endpoint)
     .query({token: this._id})
+    .query({q: this._keywords})
     .end(function (err, res) {
-      return cb(err, res.text);
+      // HTTP errors
+      if (err) return cb(err);
+
+      // Text errors
+      if (res.type === 'text/plain') return cb(new Error(res.text));
+
+      // API errors
+      if (err = formatErrors(res.body.errors)) return cb(err);
+
+      // Format results
+      var result = format(res.body.products);
+
+      return cb(null, result);
     });
+};
+
+var formatErrors = function (errors) {
+  var first = _.first(errors);
+
+  if (!first) return null;
+
+  return new Error(first.message);
+};
+
+var format = function (products) {
+  return products.map(function (p) {
+    var offer = formatOffers(p.offers)
+      , name = p.name;
+
+    if (!offer || !name) return null;
+
+    return _.extend(offer, {
+      name: name
+    });
+  })
+  .filter(function (p) {
+    return p !== null;
+  });
+};
+
+var formatOffers = function (offers) {
+  var first = _.first(offers)
+    , price = formatPrices(first.priceHistory);
+
+  if (!first || !price) return null;
+
+  return _.extend(price, {
+    url: first.productUrl,
+    id: first.id
+  })
+};
+
+var formatPrices = function (prices) {
+  var first = _.first(prices);
+
+  if (!first) return null;
+
+  return {
+    listPrice: first.price.value,
+    currency: first.price.currency
+  }
 };
