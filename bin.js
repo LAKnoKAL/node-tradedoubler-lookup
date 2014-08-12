2bc937dccaadf2b0fbf3474d3fe84eefc30b4e96
Adam Rudd <adam.rudd@uqconnect.edu.au>
2014-08-12 11:09:53 +0200

    Add feed to bin

--- bin.js
+++ bin.js
@@ -17,10 +17,14 @@ var opts = nomnom
     abbr: 'k',
     required: 'true'
   })
+  .option('feed', {
+    abbr: 'f',
+  })
   .parse();
 
 tradedoubler({keywords: opts.keywords})
   .id(opts.id)
+  .feed(opts.feed)
   .done(function (err, res) {
     if (err) throw err;
     console.log(JSON.stringify(res));
